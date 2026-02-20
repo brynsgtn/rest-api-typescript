0b738c81ed1328c0f41e48d28ea9dd263acd5b30
brynsgtn <bryansuguitan17@gmail.com>
2026-02-20 11:54:06 -0600

    Add router

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { createUser, getUsersByEmail } from '../db/users';
-import { authentication, random } from 'helpers';
+import { authentication, random } from '../helpers';
 
 export const register = async (req: express.Request, res: express.Response) => {
     try {
