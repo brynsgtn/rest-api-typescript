cf8e3901b8b0d66a2234667ce2d501c9ac68eaa6
brynsgtn <bryansuguitan17@gmail.com>
2026-02-22 13:42:58 -0600

    Add updateUser controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getUsers, deleteUserById } from '../db/users';
+import { getUsers, deleteUserById, getUserById } from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -19,6 +19,26 @@ export const deleteUser = async (req: express.Request <{ id: string }>, res: exp
         const deletedUser = await deleteUserById(id);
 
         return res.json(deletedUser);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+};
+
+export const updateUser = async (req: express.Request<{ id: string }>, res: express.Response) => {
+    try {
+        const {id} = req.params;
+        const {username} = req.body;
+
+        if(!username) {
+            return res.sendStatus(400);
+        }
+
+        const user = await getUserById(id);
+        user.username = username;
+
+        await user.save();
+        return res.status(200).json(user).end();
     } catch (error) {
         console.log(error);
         return res.sendStatus(400);
